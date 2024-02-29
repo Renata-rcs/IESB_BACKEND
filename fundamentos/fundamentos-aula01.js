9cf27b44ab33974c4d7874f99acd8b837b7e5dbf
renata-rcs <renataa.rcs@gmail.com>
2024-02-29 20:44:48 -0300

    Testando variaveis

--- fundamentos/fundamentos-aula01.js
+++ fundamentos/fundamentos-aula01.js
@@ -1,6 +1,24 @@
-console.log("TESTE")
+/*console.log("TESTE")
 console.log("TESTE1")
 console.log("TESTE2")
 console.log("TESTE3")
 console.log("TESTE4")
-console.log("TESTE5")
+console.log("TESTE5")*/
+
+/*{
+    console.log("TESTE BLOCO 1");
+}
+
+{
+    console.log("TESTE BLOCO 2");
+}
+
+{
+    console.log("TESTE BLOCO 3");
+}
+
+{
+    console.log("TESTE BLOCO 4");
+}
+*/
+
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,32 @@
+//VAR
+
+
+//var nome = "Renata Carvalho"
+//var aprovado = true
+//var notaA1 = 10
+//var notaA1 = 7
+//var notaCalculada = 6
+
+const nome = "Renata Carvalho"  //O valor não muda.
+const nota = 6
+
+//nome = "Carol"
+//var nome = "Carol"
+//console.log(nome)
+
+let aprovado = false
+//let notaCalculada = 0
+
+if(nota >= 6){
+    aprovado = true
+}
+
+
+
+
+
+console.log("Nome: ", nome)
+console.log("Nota: ", nota)
+console.log("Aprovado:", aprovado)
+
+
