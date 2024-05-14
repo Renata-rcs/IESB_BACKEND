faf0caba256bd1fbc0fe504a179133140799ebe8
Renata <renataa.rcs@gmail.com>
2024-05-14 18:07:46 -0300

    Terminando crud cargos

--- 13-api-empresas/src/controllers/CargoController.js
+++ 13-api-empresas/src/controllers/CargoController.js
@@ -1,65 +1,85 @@
 const Cargo = require('../models/Cargo')
-const CargoModel = require('../models/Cargo')
-
 
 async function create(req, res) {
-    try{
-       const cargo = new Cargo(req.body)
-       const cargoCriado = await cargo.save()
-       res.status(201).json(cargoCriado)
-    }
-    catch (error) {
+    try {
+        const cargo = new Cargo(req.body)
+        const cargoCriado = await cargo.save()
+        res.status(201).json(cargoCriado)
+    } catch (error) {
         console.error("Erro ao criar cargo: ", error)
         res.status(400).json({
             mensagem: "Erro ao criar cargo!",
-            erro: error.mensage
+            erro: error.message
         })
     }
 }
 
-
 async function getAll(req, res) {
-    res.json(await Cargo.find())
+    try {
+        const cargos = await Cargo.find()
+        res.json(cargos)
+    } catch (error) {
+        console.error("Erro ao buscar cargos: ", error)
+        res.status(500).json({
+            mensagem: "Erro ao buscar cargos!",
+            erro: error.message
+        })
+    }
 }
 
 async function getById(req, res) {
-    const cargo = await Cargo.findById(req.params.id)
-    if (cargo) {
-        res.json(cargo)
-    }
-    else {
-        res.status(404).json({monsagem: "Cargo não encontrado!"})
+    try {
+        const cargo = await Cargo.findById(req.params.id)
+        if (cargo) {
+            res.json(cargo)
+        } else {
+            res.status(404).json({ mensagem: "Cargo não encontrado!" })
+        }
+    } catch (error) {
+        console.error("Erro ao buscar cargo por ID: ", error)
+        res.status(500).json({
+            mensagem: "Erro ao buscar cargo por ID!",
+            erro: error.message
+        })
     }
 }
 
 async function update(req, res) {
     try {
-        const cargoAtualizado = await Cargo.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        res.json(cargoAtualizado)
-    }
-    catch (error) {
-        console.log("Erro ao criar cargo: ", error)
+        const cargoAtualizado = await Cargo.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (cargoAtualizado) {
+            res.json(cargoAtualizado)
+        } else {
+            res.status(404).json({ mensagem: "Cargo não encontrado!" })
+        }
+    } catch (error) {
+        console.error("Erro ao atualizar cargo: ", error)
         res.status(400).json({
-            mensagem: "Erro ao Atualizar cargo!",
-            erro: error.mensage
+            mensagem: "Erro ao atualizar cargo!",
+            erro: error.message
         })
     }
 }
 
 async function remove(req, res) {
-    await Cargo.findByIdAndDelete(req.params.id)
-    res.json({mensagem: "Excluido com sucesso!"})
+    try {
+        const cargoExcluido = await Cargo.findByIdAndDelete(req.params.id)
+        if (cargoExcluido) {
+            res.json({
+                mensagem: "Cargo excluído com sucesso!",
+                cargoExcluido
+            })
+        } else {
+            res.status(404).json({ mensagem: "Cargo não encontrado!" })
+        }
+    } catch (error) {
+        console.error("Erro ao excluir cargo: ", error)
+        res.status(400).json({
+            mensagem: "Erro ao excluir cargo!",
+            erro: error.message
+        })
+    }
 }
-
-
-
-
-
-
-
-
-
-
 
 module.exports = {
     create,
@@ -67,5 +87,4 @@ module.exports = {
     getById,
     update,
     remove
-
-} 
+}
--- 13-api-empresas/src/models/Cargo.js
+++ 13-api-empresas/src/models/Cargo.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
+
 const schema = new mongoose.Schema(
     {
         nome: {
             type: String,
+            required: true
+        },
+        descricao: {
+            type: String,
+            required: false
+        },
+        salario: {
+            type: Number,
             required: true
         }
     },
@@ -11,7 +20,5 @@ const schema = new mongoose.Schema(
         timestamps: true
     }
 )
-
 const Cargo = mongoose.model('cargo', schema)
-
 module.exports = Cargo
