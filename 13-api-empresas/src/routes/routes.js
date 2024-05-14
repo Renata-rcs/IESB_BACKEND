bc4b31d0c87df3e3b51fb4ca2ab75cffafbfd295
Renata <renataa.rcs@gmail.com>
2024-05-14 18:08:16 -0300

    Criando crud departamentos

--- 13-api-empresas/src/routes/routes.js
+++ 13-api-empresas/src/routes/routes.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
+// Controllers
 const CargoController = require('../controllers/CargoController')
-const FuncionarioController= require('../controllers/FuncionarioController')
+const FuncionarioController = require('../controllers/FuncionarioController')
+const DepartamentoController = require('../controllers/DepartamentoController')
+
+
+
+// validators
+const { validarId } = require('../validators/IdValidator')
+const { cargoValidador } = require('../validators/CargoValidator')
+const { departamentoValidador } = require('../validators/DepartamentoValidator');
 
 
 
 // Cargos
-router.post('/cargos', CargoController.create)
+router.post('/cargos', cargoValidador, CargoController.create)
 router.get('/cargos', CargoController.getAll)
-router.get('/cargos/:id', CargoController.getById)
-router.put('/cargos/:id', CargoController.update)
-router.delete('/cargos/:id', CargoController.remove)
-
-
-
+router.get('/cargos/:id', validarId, CargoController.getById)
+router.put('/cargos/:id', validarId, cargoValidador, CargoController.update)
+router.delete('/cargos/:id', validarId, CargoController.remove)
 
 
 // Funcionarios
@@ -23,6 +29,14 @@ router.get('/funcionarios/:id', FuncionarioController.getById)
 router.get('/funcionarios', FuncionarioController.getAll)
 router.put('/funcionarios/:id', FuncionarioController.update)
 router.delete('/funcionarios/:id', FuncionarioController.remove)
+
+
+// Departamentos
+router.post('/departamentos', departamentoValidador, DepartamentoController.create);
+router.get('/departamentos', DepartamentoController.getAll);
+router.get('/departamentos/:id', validarId, DepartamentoController.getById);
+router.put('/departamentos/:id', validarId, departamentoValidador, DepartamentoController.update);
+router.delete('/departamentos/:id', validarId, DepartamentoController.remove);
 
 
 module.exports = router
