7682efc1455f6fcda7c09161e234c9518d7e98bf
Renata <renataa.rcs@gmail.com>
2024-05-17 16:49:46 -0300

    criando crud tarefa e projeto

--- 13-api-empresas/src/routes/routes.js
+++ 13-api-empresas/src/routes/routes.js
@@ -1,35 +1,34 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // Controllers
-const CargoController = require('../controllers/CargoController')
-const FuncionarioController = require('../controllers/FuncionarioController')
-const DepartamentoController = require('../controllers/DepartamentoController')
+const CargoController = require('../controllers/CargoController');
+const FuncionarioController = require('../controllers/FuncionarioController');
+const DepartamentoController = require('../controllers/DepartamentoController');
+const TarefaController = require('../controllers/TarefaController');
+const ProjetoController = require('../controllers/ProjetoController');
 
-
-
-// validators
-const { validarId } = require('../validators/IdValidator')
-const { cargoValidador } = require('../validators/CargoValidator')
+// Validators
+const { validarId } = require('../validators/IdValidator');
+const { funcionarioValidador } = require('../validators/FuncionarioValidator');
+const { cargoValidador } = require('../validators/CargoValidator');
 const { departamentoValidador } = require('../validators/DepartamentoValidator');
-
-
+const { projetoValidador } = require('../validators/ProjetoValidator');
+const { tarefaValidador } = require('../validators/TarefaValidator');
 
 // Cargos
-router.post('/cargos', cargoValidador, CargoController.create)
-router.get('/cargos', CargoController.getAll)
-router.get('/cargos/:id', validarId, CargoController.getById)
-router.put('/cargos/:id', validarId, cargoValidador, CargoController.update)
-router.delete('/cargos/:id', validarId, CargoController.remove)
-
+router.post('/cargos', cargoValidador, CargoController.create);
+router.get('/cargos', CargoController.getAll);
+router.get('/cargos/:id', validarId, CargoController.getById);
+router.put('/cargos/:id', validarId, cargoValidador, CargoController.update);
+router.delete('/cargos/:id', validarId, CargoController.remove);
 
 // Funcionarios
-router.post('/funcionarios', FuncionarioController.create)
-router.get('/funcionarios/:id', FuncionarioController.getById)
-router.get('/funcionarios', FuncionarioController.getAll)
-router.put('/funcionarios/:id', FuncionarioController.update)
-router.delete('/funcionarios/:id', FuncionarioController.remove)
-
+router.post('/funcionarios', funcionarioValidador, FuncionarioController.create);
+router.get('/funcionarios', FuncionarioController.getAll);
+router.get('/funcionarios/:id', validarId, FuncionarioController.getById);
+router.put('/funcionarios/:id', validarId, funcionarioValidador, FuncionarioController.update);
+router.delete('/funcionarios/:id', validarId, FuncionarioController.remove);
 
 // Departamentos
 router.post('/departamentos', departamentoValidador, DepartamentoController.create);
@@ -38,5 +37,18 @@ router.get('/departamentos/:id', validarId, DepartamentoController.getById);
 router.put('/departamentos/:id', validarId, departamentoValidador, DepartamentoController.update);
 router.delete('/departamentos/:id', validarId, DepartamentoController.remove);
 
+// Projetos
+router.post('/projetos', projetoValidador, ProjetoController.create);
+router.get('/projetos', ProjetoController.getAll);
+router.get('/projetos/:id', validarId, ProjetoController.getById);
+router.put('/projetos/:id', validarId, projetoValidador, ProjetoController.update);
+router.delete('/projetos/:id', validarId, ProjetoController.remove);
 
-module.exports = router
+// Tarefas
+router.post('/tarefas', tarefaValidador, TarefaController.create);
+router.get('/tarefas', TarefaController.getAll);
+router.get('/tarefas/:id', validarId, TarefaController.getById);
+router.put('/tarefas/:id', validarId, tarefaValidador, TarefaController.update);
+router.delete('/tarefas/:id', validarId, TarefaController.remove);
+
+module.exports = router;
